44e5b452e391f3f4285bc8715d03796bb643bdb1
Muthukumaran M <muthukumaran.m@justanswer.com>
2020-07-02 02:27:47 +0530

    Created a new API Test using SpecFlowBDD

---
+++ SpecFlowBDDAPITest/SpecFlowBDDAPITest/Features/APIStatusCode.feature
@@ -0,0 +1,11 @@
+﻿Feature: APIStatusCode
+	Get the API Status Code
+	As a user
+	I want to get the status code of ap
+
+@mytag
+Scenario: Get the Status Code 200 for the given api
+	Given API End Point
+	And pass the method as Get
+	When I press send
+	Then the Status Code should be OK/200  on API Response
---
+++ SpecFlowBDDAPITest/SpecFlowBDDAPITest/StepDefinition/APIStatusCode_StepDef.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowBDDAPITest.StepDefinition
+{
+	[Binding]
+	public sealed class APIStatusCode_StepDef
+	{
+		// For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+		private readonly ScenarioContext context;
+
+		public APIStatusCode_StepDef(ScenarioContext injectedContext)
+		{
+			context = injectedContext;
+		}
+
+        [Given(@"API End Point")]
+        public void GivenAPIEndPoint()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"pass the method as Get")]
+        public void GivenPassTheMethodAsGet()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I press send")]
+        public void WhenIPressSend()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the Status Code should be OK/(.*)  on API Response")]
+        public void ThenTheStatusCodeShouldBeOKOnAPIResponse(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
